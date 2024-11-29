8dba45527a169de78311111d22830891d87da6e7
Stephan Paul <stephanpauld@gmail.com>
2024-11-29 14:33:24 -0700

    back bi vids added

--- src/data/workouts.ts
+++ src/data/workouts.ts
@@ -26,274 +26,282 @@ export const workoutPlan: WorkoutPlan = {
     supersets: [
       {
         exercises: [
-          { 
-            name: "Dumbbell Bench Press", 
-            sets: 4, 
+          {
+            name: "Dumbbell Bench Press",
+            sets: 4,
             reps: "8-12",
-            videoUrl: undefined // Add your YouTube URL here
+            videoUrl: undefined, // Add your YouTube URL here
           },
-          { 
-            name: "Dumbbell Flyes", 
-            sets: 4, 
+          {
+            name: "Dumbbell Flyes",
+            sets: 4,
             reps: "12-15",
-            videoUrl: undefined
-          }
-        ]
+            videoUrl: undefined,
+          },
+        ],
       },
       {
         exercises: [
-          { 
-            name: "Incline Dumbbell Press", 
-            sets: 3, 
+          {
+            name: "Incline Dumbbell Press",
+            sets: 3,
             reps: "10-12",
-            videoUrl: undefined
+            videoUrl: undefined,
           },
-          { 
-            name: "Push-Ups on Bench", 
-            sets: 3, 
+          {
+            name: "Push-Ups on Bench",
+            sets: 3,
             reps: "failure",
-            videoUrl: undefined
-          }
-        ]
+            videoUrl: undefined,
+          },
+        ],
       },
       {
         exercises: [
-          { 
-            name: "Dumbbell Skull Crushers", 
-            sets: 3, 
+          {
+            name: "Dumbbell Skull Crushers",
+            sets: 3,
             reps: "10-12",
-            videoUrl: undefined
+            videoUrl: undefined,
           },
-          { 
-            name: "Dumbbell Close-Grip Bench Press", 
-            sets: 3, 
+          {
+            name: "Dumbbell Close-Grip Bench Press",
+            sets: 3,
             reps: "10-12",
-            videoUrl: undefined
-          }
-        ]
-      }
+            videoUrl: undefined,
+          },
+        ],
+      },
     ],
-    finisher: ["Bench Dips to Failure - 3 sets", "Dumbbell Pull-Over - 12-15 reps"]
+    finisher: [
+      "Bench Dips to Failure - 3 sets",
+      "Dumbbell Pull-Over - 12-15 reps",
+    ],
   },
   "Day 2": {
     focus: "Back and Biceps",
     supersets: [
       {
         exercises: [
-          { 
-            name: "Dumbbell Bent-Over Rows", 
-            sets: 4, 
+          {
+            name: "Dumbbell Bent-Over Rows",
+            sets: 4,
             reps: "8-12",
-            videoUrl: undefined
+            videoUrl: "https://www.youtube.com/embed/6gvmcqr226U",
           },
-          { 
-            name: "Dumbbell Pull-Over", 
-            sets: 4, 
+          {
+            name: "Dumbbell Pull-Over",
+            sets: 4,
             reps: "12-15",
-            videoUrl: undefined
-          }
-        ]
+            videoUrl: "https://www.youtube.com/embed/FK4rHfWKEac",
+          },
+        ],
       },
       {
         exercises: [
-          { 
-            name: "One-Arm Dumbbell Row", 
-            sets: 3, 
+          {
+            name: "One-Arm Dumbbell Row",
+            sets: 3,
             reps: "10-12",
-            videoUrl: undefined
+            videoUrl: "https://www.youtube.com/embed/dFzUjzfih7k",
           },
-          { 
-            name: "Dumbbell Reverse Flyes", 
-            sets: 3, 
+          {
+            name: "Dumbbell Reverse Flyes",
+            sets: 3,
             reps: "12-15",
-            videoUrl: undefined
-          }
-        ]
+            videoUrl: "https://www.youtube.com/embed/hf7jnF45N_I",
+          },
+        ],
       },
       {
         exercises: [
-          { 
-            name: "Dumbbell Bicep Curls", 
-            sets: 3, 
+          {
+            name: "Dumbbell Bicep Curls",
+            sets: 3,
             reps: "10-12",
-            videoUrl: undefined
+            videoUrl: "https://www.youtube.com/embed/HnHuhf4hEWY",
           },
-          { 
-            name: "Dumbbell Hammer Curls", 
-            sets: 3, 
+          {
+            name: "Dumbbell Hammer Curls",
+            sets: 3,
             reps: "10-12",
-            videoUrl: undefined
-          }
-        ]
-      }
+            videoUrl: "https://www.youtube.com/embed/fM0TQLoesLs",
+          },
+        ],
+      },
     ],
-    finisher: ["Dumbbell Concentration Curls - 3 sets to failure"]
+    finisher: ["Dumbbell Concentration Curls - 3 sets to failure"],
   },
   "Day 3": {
     focus: "Legs and Abs",
     supersets: [
       {
         exercises: [
-          { 
-            name: "Dumbbell Goblet Squats", 
-            sets: 4, 
+          {
+            name: "Dumbbell Goblet Squats",
+            sets: 4,
             reps: "12-15",
-            videoUrl: undefined
+            videoUrl: undefined,
           },
-          { 
-            name: "Dumbbell Walking Lunges", 
-            sets: 4, 
+          {
+            name: "Dumbbell Walking Lunges",
+            sets: 4,
             reps: "12 per leg",
-            videoUrl: undefined
-          }
-        ]
+            videoUrl: undefined,
+          },
+        ],
       },
       {
         exercises: [
-          { 
-            name: "Dumbbell Romanian Deadlifts", 
-            sets: 3, 
+          {
+            name: "Dumbbell Romanian Deadlifts",
+            sets: 3,
             reps: "10-12",
-            videoUrl: undefined
+            videoUrl: undefined,
           },
-          { 
-            name: "Dumbbell Step-Ups", 
-            sets: 3, 
+          {
+            name: "Dumbbell Step-Ups",
+            sets: 3,
             reps: "12 per leg",
-            videoUrl: undefined
-          }
-        ]
+            videoUrl: undefined,
+          },
+        ],
       },
       {
         exercises: [
-          { 
-            name: "Dumbbell Sumo Squats", 
-            sets: 3, 
+          {
+            name: "Dumbbell Sumo Squats",
+            sets: 3,
             reps: "15",
-            videoUrl: undefined
+            videoUrl: undefined,
           },
-          { 
-            name: "Dumbbell Calf Raises", 
-            sets: 3, 
+          {
+            name: "Dumbbell Calf Raises",
+            sets: 3,
             reps: "15-20",
-            videoUrl: undefined
-          }
-        ]
-      }
+            videoUrl: undefined,
+          },
+        ],
+      },
     ],
     abs: [
       {
-        superset: ["Dumbbell Russian Twists - 12 reps per side", "Weighted Leg Raises - 15 reps"]
-      }
+        superset: [
+          "Dumbbell Russian Twists - 12 reps per side",
+          "Weighted Leg Raises - 15 reps",
+        ],
+      },
     ],
-    finisher: ["5 sets of 20-second squat jumps with 10-second rest"]
+    finisher: ["5 sets of 20-second squat jumps with 10-second rest"],
   },
   "Day 4": {
     focus: "Shoulders and Triceps",
     supersets: [
       {
         exercises: [
-          { 
-            name: "Dumbbell Shoulder Press", 
-            sets: 4, 
+          {
+            name: "Dumbbell Shoulder Press",
+            sets: 4,
             reps: "8-12",
-            videoUrl: undefined
+            videoUrl: undefined,
           },
-          { 
-            name: "Dumbbell Lateral Raises", 
-            sets: 4, 
+          {
+            name: "Dumbbell Lateral Raises",
+            sets: 4,
             reps: "12-15",
-            videoUrl: undefined
-          }
-        ]
+            videoUrl: undefined,
+          },
+        ],
       },
       {
         exercises: [
-          { 
-            name: "Dumbbell Front Raises", 
-            sets: 3, 
+          {
+            name: "Dumbbell Front Raises",
+            sets: 3,
             reps: "12-15",
-            videoUrl: undefined
+            videoUrl: undefined,
           },
-          { 
-            name: "Dumbbell Rear Delt Flyes", 
-            sets: 3, 
+          {
+            name: "Dumbbell Rear Delt Flyes",
+            sets: 3,
             reps: "12-15",
-            videoUrl: undefined
-          }
-        ]
+            videoUrl: undefined,
+          },
+        ],
       },
       {
         exercises: [
-          { 
-            name: "Dumbbell Tricep Kickbacks", 
-            sets: 3, 
+          {
+            name: "Dumbbell Tricep Kickbacks",
+            sets: 3,
             reps: "12-15",
-            videoUrl: undefined
+            videoUrl: undefined,
           },
-          { 
-            name: "Dumbbell Overhead Tricep Extension", 
-            sets: 3, 
+          {
+            name: "Dumbbell Overhead Tricep Extension",
+            sets: 3,
             reps: "10-12",
-            videoUrl: undefined
-          }
-        ]
-      }
+            videoUrl: undefined,
+          },
+        ],
+      },
     ],
-    finisher: ["Dumbbell Upright Row - 3 sets to failure"]
+    finisher: ["Dumbbell Upright Row - 3 sets to failure"],
   },
   "Day 5": {
     focus: "Full Body (High-Intensity)",
     supersets: [
       {
         exercises: [
-          { 
-            name: "Dumbbell Thrusters", 
-            sets: 4, 
+          {
+            name: "Dumbbell Thrusters",
+            sets: 4,
             reps: "10-12",
-            videoUrl: undefined
+            videoUrl: undefined,
           },
-          { 
-            name: "Dumbbell Renegade Rows", 
-            sets: 4, 
+          {
+            name: "Dumbbell Renegade Rows",
+            sets: 4,
             reps: "12 per arm",
-            videoUrl: undefined
-          }
-        ]
+            videoUrl: undefined,
+          },
+        ],
       },
       {
         exercises: [
-          { 
-            name: "Dumbbell Goblet Squats", 
-            sets: 3, 
+          {
+            name: "Dumbbell Goblet Squats",
+            sets: 3,
             reps: "15",
-            videoUrl: undefined
+            videoUrl: undefined,
           },
-          { 
-            name: "Dumbbell Push-Ups", 
-            sets: 3, 
+          {
+            name: "Dumbbell Push-Ups",
+            sets: 3,
             reps: "failure",
-            videoUrl: undefined
-          }
-        ]
+            videoUrl: undefined,
+          },
+        ],
       },
       {
         exercises: [
-          { 
-            name: "Dumbbell Burpees", 
-            sets: 3, 
+          {
+            name: "Dumbbell Burpees",
+            sets: 3,
             reps: "10",
-            videoUrl: undefined
+            videoUrl: undefined,
           },
-          { 
-            name: "Dumbbell Farmers Walk", 
-            sets: 3, 
+          {
+            name: "Dumbbell Farmers Walk",
+            sets: 3,
             reps: "1-minute",
-            videoUrl: undefined
-          }
-        ]
-      }
+            videoUrl: undefined,
+          },
+        ],
+      },
     ],
-    finisher: ["5-minute AMRAP: 10 Dumbbell Swings, 10 Dumbbell Step-Ups, 10 Dumbbell Bicep Curls"]
-  }
+    finisher: [
+      "5-minute AMRAP: 10 Dumbbell Swings, 10 Dumbbell Step-Ups, 10 Dumbbell Bicep Curls",
+    ],
+  },
 };
